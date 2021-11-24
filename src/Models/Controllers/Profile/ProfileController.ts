da8141c077e4514e6c6e629c06fc2d1c9341ad13
caliLai <calilai2018@gmail.com>
2021-11-23 23:58:57 -0800

    im too tired but in theory, the endpoint will work

--- src/Models/Controllers/Profile/ProfileController.ts
+++ src/Models/Controllers/Profile/ProfileController.ts
@@ -1,6 +1,7 @@
 // file to handle routing stuff for profiles and their functions
 
 import IUser from "../../Interfaces/Profile/IProfile";
+import IReview from "../../Interfaces/Profile/IReview";
 
 import express, {Router, Request} from "express";
 import View from "./View";
@@ -18,6 +19,7 @@ class ProfileController {
 		this.router.get(`${this.path}/view/:id`, this.view);
 		this.router.get(`${this.path}/reviews/:id/getAll`, this.getLandlordReviews);
 		this.router.get(`${this.path}/reviews/:id/count`, this.count);
+		this.router.get(`${this.path}/reviews/create`, this.create);
 	}
 	//viewing a profile that (ideally) isn't your own
 	private view = async (req:express.Request, res:express.Response, next:express.NextFunction) => {
@@ -31,6 +33,28 @@ class ProfileController {
 			res.end("error occurred");
 		})
 	}
+
+	// create a review
+	private create = async(req:express.Request, res:express.Response) => {
+
+		let u = req.user as IUser;
+
+		let newReview:IReview = {
+			content: req.body.content,
+			is_recommended: req.body.is_recommended,	
+			cleanliness_rating:req.body.cleanliness_rating,
+			communication_rating:req.body.communication_rating,
+			maintenance_rating:req.body.maintenance_rating,
+			availability_rating:req.body.availability_rating,
+
+			landlord_id:req.body.landlord_id,
+			tenant_id:u.user_id
+		};
+		this._handleReviews.create(newReview)
+		.then(m => res.end(m))
+		.catch(() => res.end("error occurred"));
+	}
+
 	// obtain reviews for a single landlord; to be shown on their profile
 	private getLandlordReviews = async (req:express.Request, res:express.Response, next:express.NextFunction) => {
 		this._handleReviews.view(parseInt(req.params.id))
@@ -40,7 +64,7 @@ class ProfileController {
 			res.end("Reviews unavailable");
 		})
 	}
-
+	// count how many reviews a Landlord has
 	private count = async (req:express.Request, res:express.Response, next:express.NextFunction) => {
 		// console.log("hi");
 		this._handleReviews.count(parseInt(req.params.id))
